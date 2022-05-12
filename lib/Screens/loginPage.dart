3016bd8e93e6b9442865b34324ece2302d319929
haydenang <64570131+haydenang@users.noreply.github.com>
2022-05-13 02:41:49 +0800

    Overhaul of Login Screen
    Easier to read!

---
+++ lib/Screens/loginPage.dart
@@ -0,0 +1,127 @@
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "LoginPage",
+      home: LoginWidget(),
+    );
+  }
+}
+
+class LoginWidget extends StatefulWidget {
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget>{
+  //form key
+  final _formKey= GlobalKey<FormState>();
+
+  //editing controller
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: (){},
+      onSaved: (value){
+        emailController.text =  value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.mail),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Email",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10)
+        )
+      ),
+    );
+    final passwordField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      controller: passwordController,
+      //validator: (){},
+      onSaved: (value){
+        passwordController.text =  value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key_off_outlined),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10)
+          )
+      ),
+    );
+
+    final loginButton =  Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(16),
+      color: Colors.blueAccent,
+      shadowColor: Colors.amber,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: (){},
+        child: Text("Login",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20,color: Colors.white60,fontWeight: FontWeight.bold),),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 60, child: Text('Material Login',style: TextStyle(fontSize: 40,color:Colors.green),),),
+                  SizedBox(
+                    height: 100,
+                    child: Image.asset("assets/icons8-graduation-64.png",
+                    fit: BoxFit.fill,
+                  )),
+                  //Email Field SizedBox
+                  SizedBox(height: 45,),
+                  emailField,
+                  SizedBox(height: 25,),
+                  passwordField,
+                  SizedBox(height: 35,),
+                  loginButton,
+                  SizedBox(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Don't have an account?"),
+                      GestureDetector(onTap: (){},
+                      child: Text("SignUp", style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold, fontSize: 15,),))
+                    ],
+                  )
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tutor_me/Screens/loginPage.dart';
 
 void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      title: "LoginPage",
-      home: LoginWidget(),
-    );
-  }
-}
-
-class LoginWidget extends StatefulWidget {
-  @override
-  _LoginWidgetState createState() => _LoginWidgetState();
-}
-
-class _LoginWidgetState extends State<LoginWidget>{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            Column(
-                children: <Widget>[
-                  SizedBox(height: 80,),
-                  Image.asset('assets/icons8-login-60.png'),
-                  SizedBox(height: 40,),
-                  Text('Material Login',style: TextStyle(fontSize: 25,color:Colors.green),)
-                ]
-            ),
-            SizedBox(height: 60.0,),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Email",
-                labelStyle: TextStyle(fontSize: 20),
-                filled: true,
-              ),
-            ),
-            SizedBox(height: 20.0,),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-                labelStyle: TextStyle(fontSize: 20),
-                filled: true,
-              ),
-            ),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueAccent,
-                    onPrimary: Colors.black54,
-                    onSurface: Colors.grey,
-                    elevation: 5,
-                    textStyle: TextStyle(fontSize: 20),
-                    shadowColor: Colors.amber
-                  ),
-                  onPressed: (){print("pressed");},
-                  onLongPress: (){},
-                  child: Text("Login"),
-                    ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.blueAccent,
-                      onPrimary: Colors.black54,
-                      onSurface: Colors.grey,
-                      elevation: 5,
-                      textStyle: TextStyle(fontSize: 20),
-                      shadowColor: Colors.amber
-                  ),
-                  onPressed: (){print("Register");},
-                  onLongPress: (){},
-                  child: Text("Register"),
-                )
-                  ]
-                )
-              ],
-        ),
-      ),
+      title: 'Email and Password Login',
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      home: LoginPage(),
     );
   }
 }
