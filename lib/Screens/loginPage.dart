b7bee6ec6262e1d816ec93dac056f592e59e2375
haydenang <64570131+haydenang@users.noreply.github.com>
2022-05-13 22:25:54 +0800

    Finished Registration Page
    UI of registration page is done, fixed the pixel overflow error by adding a scrollview

--- lib/Screens/loginPage.dart
+++ lib/Screens/loginPage.dart
@@ -81,89 +81,93 @@ class _LoginWidgetState extends State<LoginWidget> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 60,
-                    child: Text(
-                      'Material Login',
-                      style: TextStyle(fontSize: 40, color: Colors.green),
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 60,
+                      child: Text(
+                        'Material Login',
+                        style: TextStyle(fontSize: 40, color: Colors.green),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                      height: 100,
-                      child: Image.asset(
-                        "assets/icons8-graduation-64.png",
-                        fit: BoxFit.fill,
-                      )),
-                  //Email Field SizedBox
-                  SizedBox(
-                    height: 45,
-                  ),
-                  emailField,
-                  SizedBox(
-                    height: 25,
-                  ),
-                  passwordField,
-                  SizedBox(
-                    height: 35,
-                  ),
-                  loginButton,
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Don't have an account?"),
-                      GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => RegistrationPage()));
-                          },
-                          child: Text(
-                            " SignUp",
-                            style: TextStyle(
-                              color: Colors.red,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                          ))
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Click here to Skip to homePage!"),
-                      GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HomePage()));
-                          },
-                          child: Text(
-                            " Here",
-                            style: TextStyle(
-                              color: Colors.red,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                          )),
-                    ],
-                  ),
-                ],
+                    SizedBox(
+                        height: 100,
+                        child: Image.asset(
+                          "assets/icons8-graduation-64.png",
+                          fit: BoxFit.fill,
+                        )),
+                    //Email Field SizedBox
+                    SizedBox(
+                      height: 45,
+                    ),
+                    emailField,
+                    SizedBox(
+                      height: 25,
+                    ),
+                    passwordField,
+                    SizedBox(
+                      height: 35,
+                    ),
+                    loginButton,
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Don't have an account?"),
+                        GestureDetector(
+                            //onClickListener for SignUp button to move to RegistrationPage
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          RegistrationPage()));
+                            },
+                            child: Text(
+                              " SignUp",
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ))
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Click here to Skip to homePage!"),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage()));
+                            },
+                            child: Text(
+                              " Here",
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/Screens/registrationPage.dart
+++ lib/Screens/registrationPage.dart
@@ -1,20 +1,192 @@
 import 'package:flutter/material.dart';
 import 'package:tutor_me/Screens/loginPage.dart';
 
-class RegistrationPage extends StatefulWidget{
+class RegistrationPage extends StatefulWidget {
   const RegistrationPage({Key? key}) : super(key: key);
 
   @override
   _RegistrationPageState createState() => _RegistrationPageState();
 }
 
-class _RegistrationPageState extends State<RegistrationPage>{
+class _RegistrationPageState extends State<RegistrationPage> {
+  //Form key
+  final _formKey = GlobalKey<FormState>();
+
+  //editing Controller
+  final firstNameEditingController = new TextEditingController();
+  final secondNameEditingController = new TextEditingController();
+  final emailEditingController = new TextEditingController();
+  final passwordEditingController = new TextEditingController();
+  final confirmPasswordEditingController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    //Declaration of Text Fields
+    final firstNameField = TextFormField(
+      autofocus: false,
+      controller: firstNameEditingController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: (){},
+      onSaved: (value) {
+        firstNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "First Name",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    final secondNameField = TextFormField(
+      autofocus: false,
+      controller: secondNameEditingController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: (){},
+      onSaved: (value) {
+        secondNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Surname",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailEditingController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: (){},
+      onSaved: (value) {
+        emailEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordEditingController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: (){},
+      onSaved: (value) {
+        passwordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    final confirmField = TextFormField(
+      autofocus: false,
+      controller: confirmPasswordEditingController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: (){},
+      onSaved: (value) {
+        confirmPasswordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Confirm your Password",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    //Register Button
+    final registerButoon = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(16),
+      color: Colors.blueAccent,
+      shadowColor: Colors.amber,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {},
+        child: Text(
+          "Register",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20, color: Colors.white60, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+
     // TODO: implement build
-    return Container(
-      color: Colors.white,
-      child: Text("RegisterPage"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.grey,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 100,
+                        child: Image.asset(
+                          "assets/icons8-graduation-64.png",
+                          fit: BoxFit.fill,
+                        )),
+                    //Email Field SizedBox
+                    SizedBox(
+                      height: 45,
+                    ),
+                    firstNameField,
+                    SizedBox(
+                      height: 25,
+                    ),
+                    secondNameField,
+                    SizedBox(
+                      height: 25,
+                    ),
+                    emailField,
+                    SizedBox(
+                      height: 25,
+                    ),
+                    passwordField,
+                    SizedBox(
+                      height: 25,
+                    ),
+                    confirmField,
+                    SizedBox(
+                      height: 35,
+                    ),
+                    registerButoon,
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
