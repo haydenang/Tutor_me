d52e1988833a3ade03a74d9e9cac93e518a50638
anirhc <anirhc@gmail.com>
2022-05-14 18:12:45 +0800

    Redo Upload Page

--- lib/upload.dart
+++ lib/upload.dart
@@ -1,11 +1,9 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
-import 'package:tutor_me/Screens/classesEnrolled.dart';
-import 'package:tutor_me/input_field.dar.dart';
 
-import 'button.dart';
+void main()  {
+  runApp(MyUpload());
+}
 
 class MyUpload extends StatefulWidget {
   const MyUpload({Key? key}) : super(key: key);
@@ -15,160 +13,87 @@ class MyUpload extends StatefulWidget {
 }
 
 class _MyUploadState extends State<MyUpload> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _locationController = TextEditingController();
-  final TextEditingController _sizeController = TextEditingController();
-
-  DateTime _selectedDate = DateTime.now();
-  String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
-  String _endTime = "10:30 PM";
-
-  final DatabaseRef = FirebaseDatabase.instance.reference();
+  var namecontroller = new TextEditingController();
+  var sizecontroller = new TextEditingController();
+  var datecontroller = new TextEditingController();
+  var locationcontroller = new TextEditingController();
+  var timecontroller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      appBar: AppBar(),
-      body: Container(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Add Class",
-                style: headingStyle,
-              ),
-              MyInputField(title: "Title",
-                  hint: "Enter the name of your class",
-                  widget: widget, controller: _titleController,),
-              MyInputField(title: "Location",
-                  hint: "Enter the Location",
-                  widget: widget,controller: _locationController,),
-              MyInputField(title: "Class size",
-                  hint: "Enter the class size",
-                  widget: widget, controller: _sizeController,),
-              MyInputField(
-                title: "Date", hint: DateFormat.yMd().format(_selectedDate),
-                widget: IconButton(
-                  icon: const Icon(Icons.calendar_today_outlined,
-                      color: Colors.grey),
-                  onPressed: () {
-                    _getDateFromUser();
-                  },
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                      child: MyInputField(title: "Start Time", hint: _startTime, widget: IconButton(
-                        onPressed: (){
-                          _getTimeFromUser(true);
-                        },
-                        icon: const Icon(
-                          Icons.access_time_rounded,
-                          color: Colors.grey,
-                        ),
-                      ) ,)),
-                  SizedBox(width: 12,),
-                  Expanded(
-                      child: MyInputField(title: "End Time", hint: _endTime, widget: IconButton(
-                        onPressed: (){
-                          _getTimeFromUser(false);
-                        },
-                        icon: const Icon(
-                          Icons.access_time_rounded,
-                          color: Colors.grey,
-                        ),
-                      ) ,
-                      ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 18,),
-              Row(
-                children: [
-                  MyButton(label:"Create Class", onTap: ()=> _validateData())
-                    ],
-                  )
-                ],
-          ),
-        )
-      )
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          resizeToAvoidBottomInset: false,
+      body: SafeArea(child: Column(children: [
+        Text(
+        "Add Classes",
+        style: TextStyle(fontSize: 28),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        TextFormField(
+          controller: namecontroller,
+          decoration: InputDecoration(
+              labelText: 'Class Name',border: OutlineInputBorder()),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        TextFormField(
+          controller: sizecontroller,
+          decoration: InputDecoration(
+              labelText: 'Class Size',border: OutlineInputBorder()),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        TextFormField(
+          controller: datecontroller,
+          decoration: InputDecoration(
+              labelText: 'Date',border: OutlineInputBorder()),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        TextFormField(
+          controller: locationcontroller,
+          decoration: InputDecoration(
+              labelText: 'Location',border: OutlineInputBorder()),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        TextFormField(
+          controller: timecontroller,
+          decoration: InputDecoration(
+              labelText: 'Time',border: OutlineInputBorder()),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        OutlinedButton(onPressed: () {
+          if(namecontroller.text.isNotEmpty && sizecontroller.text.isNotEmpty && datecontroller.text.isNotEmpty && locationcontroller.text.isNotEmpty && timecontroller.text.isNotEmpty){
+            insertData(namecontroller.text, sizecontroller.text, datecontroller.text, locationcontroller.text, timecontroller.text);
+          }}, child: Text(
+          "Add Class",
+          style: TextStyle(fontSize: 18),
+        ))
+      ],
+    ),
+    )
+    ),
     );
   }
 
-  _getDateFromUser() async {
-    DateTime? _pickerDate = await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime(2022),
-        lastDate: DateTime(2121)
-    );
-    if (_pickerDate != null) {
-      setState(() {
-        _selectedDate = _pickerDate;
-      });
-    }
-    else {
-      print("Its null or something is wrong");
-    }
-  }
-  _getTimeFromUser(bool isStartTime) async {
-    var pickedTime = await _showTimePicker();
-    String _formattedTime = pickedTime.format(context);
-    if(pickedTime == null){
-      print("Time cancelled");
-    }
-    else if(isStartTime == true){
-      setState(() {
-        _startTime = _formattedTime;
-      });
-    }
-    else if(isStartTime == false){
-      setState(() {
-        _endTime = _formattedTime;
-      });
-    }
-  }
-
-  _showTimePicker(){
-    return showTimePicker(
-        initialEntryMode: TimePickerEntryMode.input,
-        context: context,
-        initialTime: TimeOfDay(
-            hour: int.parse(_startTime.split(":")[0]),
-            minute: int.parse((_startTime.split(":")[1].split(" ")[0]),
-            )
-        )
-    );
-  }
-
-  _validateData(){
-    if(_titleController.text.isNotEmpty&&_locationController.text.isNotEmpty&&_sizeController.text.isNotEmpty){
-      _addUploadToDb(_titleController.text,_locationController.text,_sizeController.text,_selectedDate.toString(),_startTime.toString(),_endTime.toString());
-      Get.back();
-    }
-    else if(_titleController.text.isEmpty || _locationController.text.isEmpty || _sizeController.text.isEmpty){
-      Get.snackbar("Required", "All fields are required!",
-          snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: Colors.white,
-          colorText: Colors.red,
-        icon: const Icon(Icons.warning_amber_rounded)
-      );
-    }
-  }
-
-  _addUploadToDb(String title, String location, String size, String date, String startTime, String endTime){
-    DatabaseRef.push().set({
-      "title": title,
-      "location": location,
+  Future<void> insertData(String name, String size, String date, String location, String time) async {
+    await FirebaseFirestore.instance.collection("listing").doc('default').set({
+      "name": name,
       "size": size,
       "date": date,
-      "startTime" : startTime,
-      "endTime":endTime
+      "location": location,
+      "time" : time
     });
-
-}
+  }
 }
